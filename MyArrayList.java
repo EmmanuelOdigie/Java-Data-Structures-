b22a4ab29a92593293d934cfed78298018268971
EmmanuelOdigie <emmanuelodigie1@outlook.com>
2025-08-31 13:09:19 +0100

    Add files via upload

--- MyArrayList.java
+++ MyArrayList.java
@@ -1,25 +1,20 @@
-package JavaArrayList; // project created when setting up eclipse
+// Java Data Structures: ArrayList //
+
+package JavaDataStructures; // Project package
 
 import java.util.ArrayList; // imported ArrayList class
 
-public class MyArrayList {// created public class myArrayList
+public class MyArrayList { // created class MyArrayList
 
 	public static void main(String[] args) {
-		
-	// Created an object from the imported class 'ArrayList' named 'myList'
-		ArrayList<String> myList = new ArrayList<String>();
-	
-	// used object 'myList' to call onto the '.add' function/method to add txt to the object
-		myList.add("Volvo");
-		myList.add("Lada");
-		myList.add("Ford");
-		myList.add("Nissan");
-		myList.add("Mitzibushi");
-	
-	// Set Audi to be the first car to be displayed on the list
-		myList.add(0, "Audi"); // in Java the number starts from 0 which is the 1'st number
-		
-		System.out.println(myList); // display the information in myList object
+		ArrayList<String> cars = new ArrayList<String>(); // created an object 'cars' from imported ArrayList class
+	// used object 'cars' to call onto the .add function to add the string names of cars
+		cars.add("Volvo "); // added name 'volvo' in brackets with quotations " "
+		cars.add("Mazda ");// added name 'mazda' in brackets with quotations marks " "
+		cars.add("BMW ");// added name 'bmw' in brackets with quotations marks " "
+		cars.add("Volkswagon ");// added name 'volkswagon' in brackets with quotations marks " "
+		cars.add("Mercedese ");// added name 'mercedese' in brackets with quotations marks " "
+		System.out.println(cars); // display names above stored in the 'cars' object
 		
 
 	}
